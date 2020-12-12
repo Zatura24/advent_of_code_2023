781faea76022dc6c65484503b1a1389559c0d0fa
Jannick Joosten <jannick.joosten@gmail.com>
2020-12-12 16:01:25 +0100

    day11 small changes

--- day11/day11.py
+++ day11/day11.py
@@ -1,17 +1,24 @@
-import numpy as np
-
-character_mapping = {
-    '.': -1,
-    'L': 0,
-    '#': 1
-}
-
 with open('input', 'r') as input_file:
+    character_mapping = {
+        '.': -1,
+        'L': 0,
+        '#': 1
+    }
     input_list = [[character_mapping[space] for space in row] for row in input_file.read().splitlines()]
 
-
 rows, cols = len(input_list), len(input_list[0])
+
 star_vertices = []
+"""challenge 1"""
+# star_vertices.append([(-1, 0)])  # up
+# star_vertices.append([(1, 0)])  # down
+# star_vertices.append([(0, -1)])  # left
+# star_vertices.append([(0, 1)])  # right
+# star_vertices.append([(-1, -1)])  # up left
+# star_vertices.append([(-1, 1)])  # up right
+# star_vertices.append([(1, -1)])  # down left
+# star_vertices.append([(1, 1)])  # down right
+"""challenge 2"""
 star_vertices.append([(-x, 0) for x in range(1, 1+rows)])  # up
 star_vertices.append([(x, 0) for x in range(1, 1+rows)])  # down
 star_vertices.append([(0, -x) for x in range(1, 1+cols)])  # left
@@ -41,7 +48,11 @@ def update(grid):
 
             if grid[row][col] == 0 and total == 0:
                 new_grid[row][col] = 1
-            elif grid[row][col] == 1 and total >= 5:
+            """challenge 1"""
+            # if grid[row][col] == 1 and total >= 4: 
+            #     new_grid[row][col] = 0
+            """challenge 2"""
+            if grid[row][col] == 1 and total >= 5: 
                 new_grid[row][col] = 0
 
     return new_grid
@@ -51,7 +62,7 @@ did_update = True
 iterations = 0
 while did_update:
     new_input_list = update(input_list)
-    if np.array_equal(new_input_list, input_list):
+    if new_input_list == input_list:
         did_update = False
     else:
         iterations += 1
