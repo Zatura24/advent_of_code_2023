0bc71464baebad73f3ae960f0fc58a784bb49c8f
Jannick Joosten <jannick.joosten@gmail.com>
2022-12-12 00:24:07 +0100

    Day 11 - reduce complexity and increase performance

--- src/day11/day11.clj
+++ src/day11/day11.clj
@@ -3,35 +3,39 @@
    [clojure.string :as str]))
 
 (def puzzle (atom 1))
+(def common-divisor (atom 0))
+
+(defn eval-operation [operation]
+  (-> (str "(fn [old] (" (re-find #"[\+\*] \w+" operation) " old))")
+      (read-string)
+      (eval)))
 
 (defn parse-input
   "parses every monkey block into a monkey map
    and calculates a common divisor by multiplying all test values (which are prime)
   
    example result:
-   ```{:items [79 98]
-       :operation #(* % 19)
-       :test 23
-       :if-true 2
-       :if-false 3
-       :inspect-count 0
-       :divisor 96577}```"
+   ```
+   {:items [79 98]
+    :operation (fn [old] (* 19 old))
+    :test 23
+    :if-true 2
+    :if-false 3
+    :inspect-count 0}```"
   [monkeys]
   (let [monkeys (mapv
                  (fn [monkey]
                    (let [[_ starting-items operation test if-true if-false] (str/split-lines monkey)]
                      {:items (mapv #(Integer/parseInt %) (re-seq #"\d+" starting-items))
-                      :operation #((case (re-find #"\+|\*" operation) "+" + "*" *)
-                                   %
-                                   (Integer/parseInt (or (re-find #"\d+" operation) (str %))))
+                      :operation (eval-operation operation)
                       :test (Integer/parseInt (re-find #"\d+" test))
                       :if-true (Integer/parseInt (re-find #"\d+" if-true))
                       :if-false (Integer/parseInt (re-find #"\d+" if-false))
                       :inspect-count 0}))
-                 monkeys)
-        ;; Part 2: https://www.reddit.com/r/adventofcode/comments/zifqmh/comment/izsk4xl
-        common-divisor (reduce #(* %1 (:test %2)) 1 monkeys)]
-    (mapv #(assoc % :divisor common-divisor) monkeys)))
+                 monkeys)]
+;; Part 2: https://www.reddit.com/r/adventofcode/comments/zifqmh/comment/izsk4xl
+    (reset! common-divisor (reduce #(* %1 (:test %2)) 1 monkeys))
+    monkeys))
 
 (defn read-input []
   (->
@@ -40,24 +44,25 @@
    (parse-input)))
 
 (defn monkey-turn [monkeys index]
-  (reduce
-   (fn [monkeys item]
-     (let [{:keys [operation test if-true if-false divisor]} (get monkeys index)
-           new (if (= 1 @puzzle)
-                 (int (/ (operation item) 3))
-                 (mod (operation item) divisor))]
-       (-> monkeys
-           (update-in [(if (zero? (mod new test)) if-true if-false) :items] conj new)
-           (update-in [index :items] #(into [] (rest %)))
-           (update-in [index :inspect-count] inc))))
-   monkeys
-   (get-in monkeys [index :items])))
+  (let [puzzle1? (= 1 @puzzle)
+        divisor @common-divisor
+        {:keys [items operation test if-true if-false]} (get monkeys index)]
+    (reduce
+     (fn [monkeys item]
+       (let [new (operation item)
+             new (if puzzle1? (int (/ new 3)) (mod new divisor))]
+         (update-in
+          monkeys
+          [(if (zero? (mod new test)) if-true if-false) :items]
+          conj
+          new)))
+     (-> monkeys
+         (assoc-in [index :items] [])
+         (update-in [index :inspect-count] + (count items)))
+     items)))
 
 (defn monkey-round [monkeys]
-  (reduce
-   monkey-turn
-   monkeys
-   (range (count monkeys))))
+  (reduce monkey-turn monkeys (range (count monkeys))))
 
 (defn part-1 []
   (reset! puzzle 1)
