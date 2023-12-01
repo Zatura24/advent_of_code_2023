a1eb240b3d59658ea68d9a6f4ec3d4d1e077fa74
Jannick Joosten <jannick.joosten@gmail.com>
2023-12-01 17:59:00 +0100

    CLJ - Fix Day 1 part 2 for other inputs then mine

--- src/day1/day1.clj
+++ src/day1/day1.clj
@@ -4,7 +4,7 @@
 
 (defn part-1 []
   (->> (utils/read-input)
-       (str/split-lines)
+       str/split-lines
        (map (comp 
               utils/parse-int 
               (partial apply str) 
@@ -13,28 +13,56 @@
        (apply +)))
 
 (defn part-2 []
-  (let [parse-nums #(get {"one" "1"
-                          "two" "2"
-                          "three" "3"
-                          "four" "4"
-                          "five" "5"
-                          "six" "6"
-                          "seven" "7"
-                          "eight" "8"
-                          "nine" "9"}
+  (let [parse-nums #(get {"one"    "1"
+                          "two"    "2" 
+                          "three"  "3"
+                          "four"   "4" 
+                          "five"   "5"
+                          "six"    "6" 
+                          "seven"  "7"
+                          "eight"  "8" 
+                          "nine"   "9"}
                          %
-                         %)]
+                         %)
+        ;; use positive lookahead to not consume characters
+        nums-pattern #"(?=(\d|one|two|three|four|five|six|seven|eight|nine))"]
     (->> (utils/read-input)
-         (str/split-lines)
+         str/split-lines
          (map (comp 
                 utils/parse-int 
                 (partial apply str) 
-                (partial map parse-nums)  
-                (juxt first last)
-                (partial re-seq #"\d|one|two|three|four|five|six|seven|eight|nine")))
+                (juxt (comp parse-nums second first) 
+                      (comp parse-nums second last))
+                (partial re-seq nums-pattern)))
          (apply +))))
+
+(defn part-2-alternative []
+  (-> (utils/read-input)
+      ;; Doing this replacement/insert allows us to reuse part-1's code
+      (str/replace 
+        #"(?=(one|two|three|four|five|six|seven|eight|nine))" 
+        (comp {"one"    "1"
+               "two"    "2" 
+               "three"  "3"
+               "four"   "4" 
+               "five"   "5"
+               "six"    "6" 
+               "seven"  "7"
+               "eight"  "8" 
+               "nine"   "9"}
+              second))
+      (->>
+        str/split-lines
+        (map (comp
+               utils/parse-int 
+               (partial apply str) 
+               (juxt first last)
+               (partial re-seq #"\d")))
+        (apply +))))
 
 (comment
   (part-1)
   
-  (part-2))
+  (part-2)
+  
+  (part-2-alternative)) 
