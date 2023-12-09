4110f944a87f140e93b96d99d7d6b814b56fea3e
Jannick Joosten <jannick.joosten@gmail.com>
2023-12-09 12:58:27 +0100

    CLJ - Day 8 - small fixup

--- src/day8/day8.clj
+++ src/day8/day8.clj
@@ -2,48 +2,45 @@
   (:require [utils]
             [clojure.string :as str]))
 
-(defn parse-instructions 
+(defn parse-instructions
   "Returns a lazy (infinite!) sequence of repetitions of the instructions"
   [instructions]
   (->> instructions (map {\L 0 \R 1}) cycle))
 
 (defn parse-nodes [nodes]
   (->> nodes
-       str/split-lines
        (map (partial re-seq #"\w+"))
        (reduce
          (fn [acc [n l r]]
            (assoc acc n [l r]))
          {})))
 
-(defn count-steps 
-  "Returns a function takes the starting node and calculates the steps 
+(defn count-steps
+  "Returns a function takes the starting node and calculates the steps
    until stop-fn returns true"
   [instructions nodes stop-fn]
   (fn [starting-node]
-    (loop [[instruction & xs] (parse-instructions instructions)
+    (loop [[instruction & rest] (parse-instructions instructions)
            current-node starting-node
            steps 0]
-      (if (stop-fn current-node) 
+      (if (stop-fn current-node)
         steps
-        (recur xs (get-in nodes [current-node instruction]) (inc steps))))))
+        (recur rest (get-in nodes [current-node instruction]) (inc steps))))))
 
 (defn part-1 []
-  (let [input (utils/read-input)
-        [instructions nodes] (str/split input #"\n\n") 
+  (let [[instructions _ & nodes] (str/split-lines (utils/read-input))
         nodes (parse-nodes nodes)]
     ((count-steps instructions nodes #(= % "ZZZ")) "AAA")))
 
 (defn part-2 []
-  (let [input (utils/read-input)
-        [instructions nodes] (str/split input #"\n\n")
+  (let [[instructions _ & nodes] (str/split-lines (utils/read-input))
         nodes (parse-nodes nodes)
         ;; this is a given due to the nature of the puzzle
         ;; see: https://en.wikipedia.org/wiki/Least_common_multiple#Using_the_greatest_common_divisor
         gcd (count instructions)]
     (->> nodes
-         keys 
-         (filter #(str/starts-with? % "A"))
+         keys
+         (filter #(str/ends-with? % "A"))
          (map (count-steps instructions nodes #(str/ends-with? % "Z")))
          (reduce (fn [a x] (/ (* a x) gcd))))))
 
