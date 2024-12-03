27122f632c685d52ad2bedc4f377857a150b1830
Jannick Joosten <j.joosten@avisi.nl>
2024-12-03 09:59:05 +0100

    CLJ - Day 3 - part 1 & 2

---
+++ clojure/src/year_2024/day_03.clj
@@ -0,0 +1,28 @@
+(ns year-2024.day-03
+  (:require [utils]))
+
+(defn ^:private multiply [statement]
+  (apply * (mapv utils/parse-int (re-seq #"\d+" statement))))
+
+(defn part-1 []
+  (->> (utils/read-input)
+       (re-seq #"mul\(\d{1,3},\d{1,3}\)")
+       (transduce (map multiply) +)))
+
+(defn part-2 []
+  (->
+    (reduce
+      (fn [state statement]
+        (cond
+          (= statement "do()") (assoc state :enabled? true)
+          (= statement "don't()") (assoc state :enabled? false)
+          (:enabled? state) (assoc state :result (+ (:result state) (multiply statement)))
+          :else state))
+      {:enabled? true :result 0}
+      (re-seq #"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)" (utils/read-input)))
+    :result))
+
+(comment
+  (part-1)
+
+  (part-2))
